8d3ade92ec9de062065f48451c16189bb717e2de
Fernando Campos <camposfa@gmail.com>
2015-07-13 16:34:26 -0600

    Refinement of extra climate variables

--- analysis_scripts/bioclim.R
+++ analysis_scripts/bioclim.R
@@ -1,9 +1,65 @@
+get_bioclim_annual <- function(df, clim_extremes = NULL){
+
+  tmin_i <- df %>%
+    ungroup() %>%
+    select(site, month_of, tmin_monthly) %>%
+    spread(month_of, tmin_monthly) %>%
+    select(-site)
+
+  tmax_i <- df %>%
+    ungroup() %>%
+    select(site, month_of, tmax_monthly) %>%
+    spread(month_of, tmax_monthly) %>%
+    select(-site)
+
+  prec_i <- df %>%
+    ungroup() %>%
+    select(site, month_of, rain_monthly_mm) %>%
+    spread(month_of, rain_monthly_mm) %>%
+    select(-site)
+
+  tavg_i <- df %>%
+    ungroup() %>%
+    select(site, month_of, tavg_monthly) %>%
+    spread(month_of, tavg_monthly) %>%
+    select(-site)
+
+  clim <- clim_extremes[df$site[1], ]
+
+  b <- data.frame(suppressMessages(bioclim_annual(tmin_i, tmax_i, prec_i, tavg_i,
+                                           t.as.int = FALSE, clim = clim)))
+  names(b)[1] <- "value"
+  b$variable <- row.names(b)
+  b <- b %>% select(variable, value)
+
+  row.names(b) <- NULL
+
+  return(b)
+
+}
+
+get_indclim <- function(df, tmin, tmax, prec, tavg){
+
+  osc <- df %>%
+    ungroup() %>%
+    select(index, month_of, value) %>%
+    spread(month_of, value) %>%
+    select(-index)
+
+  i <- data.frame(suppressMessages(indclim(tmin, tmax, prec, tavg, osc, t.as.int = FALSE)))
+  i$site <- levels(climates$site)
+  i <- gather(i, variable, value, -site)
+
+  return(i)
+
+}
+
 # Code written by Jeremy VanDerWal jjvanderwal@gmail.com
 # In defunct "climates" package
 # I replaced annoying "print" command with "message", which can be suppressed
-
 bioclim <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
-          vois = 1:19, cov = FALSE, t.as.int = TRUE, period = "month")
+          vois = 1:19, cov = FALSE, t.as.int = TRUE, period = "month",
+          annual = FALSE)
 {
   tmin.vois = c(2, 3, 5, 6)
   tmax.vois = c(2, 3, 5, 7)
@@ -180,9 +236,10 @@ bioclim <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
 }
 
 
-indclim <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
-                     ind = NULL, vois = 1:9, cov = FALSE, t.as.int = TRUE,
-                     period = "month")
+
+bioclim_annual <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
+                     vois = 1:19, cov = FALSE, t.as.int = TRUE, period = "month",
+                     clim = NULL)
 {
   tmin.vois = c(2, 3, 5, 6)
   tmax.vois = c(2, 3, 5, 7)
@@ -354,6 +411,164 @@ indclim <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
       }
     }
   }
+  colnames(out) = paste("bioclim", 1:19, sep = "_")
+  return(out[, vois])
+}
+
+indclim <- function (tmin = NULL, tmax = NULL, prec = NULL, tmean = NULL,
+                     osc = NULL, vois = 1:9, t.as.int = TRUE,
+                     period = "month")
+{
+  # osc.vois = c(1:9)
+  prec.vois = c(8, 9)
+  tmean.vois = c(6, 7)
+  tsize = NULL
+  m.per.indx = function(x) {
+    c(x, (x:(x + 1))%%12 + 1)
+  }
+  w.per.indx = function(x) {
+    c(x, (x:(x + 11))%%52 + 1)
+  }
+
+  error.check = function(datum, datum.name, dsize = tsize) {
+    if (is.null(datum))
+      stop(paste(datum.name, "is needed for the variables selected"))
+    else if (is.data.frame(datum) | is.matrix(datum)) {
+      if (!(dim(datum)[2] %in% c(12, 52)))
+        stop(paste(datum.name, "must have 12 or 52 columns -- one for each month or week."))
+      dsize = c(dsize, dim(datum)[1])
+    }
+    else stop(paste(datum.name, "must be a data.frame or matrix"))
+    return(dsize)
+  }
+
+#   if (any(vois %in% osc.vois))
+#     tsize = error.check(osc, "osc")
+  if (any(vois %in% prec.vois))
+    tsize = error.check(prec, "prec")
+  if (any(vois %in% tmean.vois)) {
+    if (is.null(tmean)) {
+      tmean = (tmax + tmin)/2
+      message("Calculated tmean as (tmax+tmin)/2")
+    }
+    else tsize = error.check(tmean, "tmean")
+  }
+  if (!all(tsize == mean(tsize)))
+    stop("all input data must be of the same length")
+
+  tsize = mean(tsize)
+  out = matrix(NA, nrow = tsize, ncol = 9)
+
+  # 1. Annual mean value
+  if (any(vois == 1))
+    out[, 1] = rowMeans(osc, na.rm = T)
+
+  # 2. Seasonality
+  if (any(vois == 2)) {
+    out[, 2] = apply(osc, 1, function(x) {
+      return(sd(x, na.rm = T))
+    })
+  }
+
+  # 3. Max value
+  if (any(vois %in% c(3, 5))) {
+    out[, 3] = apply(osc, 1, function(x) {
+      return(max(x, na.rm = T))
+    })
+  }
+
+  # 4. Min value
+  if (any(vois %in% c(4, 5))) {
+    out[, 4] = apply(osc, 1, function(x) {
+      return(min(x, na.rm = T))
+    })
+  }
+
+  # 5. Annual range
+  if (any(vois == 5))
+    out[, 5] = out[, 3] - out[, 4]
+
+  if (any(vois %in% c(6:9))) {
+    if (period == "month") {
+      tt1 = matrix(NA, nrow = tsize, ncol = 12)
+      tt2 = matrix(NA, nrow = tsize, ncol = 12)
+      tt3 = matrix(NA, nrow = tsize, ncol = 12)
+      for (ii in 1:12) {
+        tt1[, ii] = rowMeans(tmean[, m.per.indx(ii)], na.rm = T)
+        tt2[, ii] = rowSums(prec[, m.per.indx(ii)], na.rm = T)
+        tt3[, ii] = rowMeans(osc[, m.per.indx(ii)], na.rm = T)
+      }
+    }
+    else {
+      tt1 = matrix(NA, nrow = tsize, ncol = 52)
+      tt2 = matrix(NA, nrow = tsize, ncol = 52)
+      tt3 = matrix(NA, nrow = tsize, ncol = 52)
+      for (ii in 1:52) {
+        tt1[, ii] = rowMeans(tmean[, w.per.indx(ii)], na.rm = T)
+        tt2[, ii] = rowSums(prec[, w.per.indx(ii)], na.rm = T)
+        tt3[, ii] = rowMeans(osc[, w.per.indx(ii)], na.rm = T)
+      }
+    }
+  }
+
+  if (any(vois %in% c(6:7))) {
+    # 6. Mean value during warmest quarter
+    if (any(vois == 6)) {
+      warmest = apply(tt1, 1, function(x) {
+        return(max(x, na.rm = T))
+      })
+      tt = NULL
+      for (ii in 1:tsize) {
+        tt = which(tt1[ii, ] == warmest[ii])
+        if (length(tt) > 1)
+          tt = tt[1]
+        out[ii, 6] = tt3[ii, tt]
+      }
+    }
+    # 7. Mean value during coldest quarter
+    if (any(vois == 7)) {
+      coldest = apply(tt1, 1, function(x) {
+        return(min(x, na.rm = T))
+      })
+      tt = NULL
+      for (ii in 1:tsize) {
+        tt = which(tt1[ii, ] == coldest[ii])
+        if (length(tt) > 1)
+          tt = tt[1]
+        out[ii, 7] = tt3[ii, tt]
+      }
+    }
+  }
+
+  if (any(vois %in% c(8:9))) {
+    # 8. Mean value during wettest quarter
+    if (any(vois == 8)) {
+      wettest = apply(tt2, 1, function(x) {
+        return(max(x, na.rm = T))
+      })
+      tt = NULL
+      for (ii in 1:tsize) {
+        tt = which(tt2[ii, ] == wettest[ii])
+        if (length(tt) > 1)
+          tt = tt[1]
+        out[ii, 8] = tt3[ii, tt]
+      }
+    }
+    # 9. Mean value during driest quarter
+    if (any(vois == 9)) {
+      driest = apply(tt2, 1, function(x) {
+        return(min(x, na.rm = T))
+      })
+      tt = NULL
+      for (ii in 1:tsize) {
+        tt = which(tt2[ii, ] == driest[ii])
+        if (length(tt) > 1)
+          tt = tt[1]
+        out[ii, 9] = tt3[ii, tt]
+      }
+    }
+  }
+
   colnames(out) = paste("indclim", 1:9, sep = "_")
   return(out[, vois])
 }
--- analysis_scripts/extra_climate_variables.R
+++ analysis_scripts/extra_climate_variables.R
@@ -1,105 +1,87 @@
-# # Calculate hottest/coldest/wettest/driest months for each site
-# monthly_clim_means <- climates %>%
-#   ungroup() %>%
-#   group_by(site, month_of) %>%
-#   summarise_each(funs(mean), rain_monthly_mm, tmin_monthly, tmax_monthly)
-#
-#
-# mon_extremes <- function(df){
-#
-#   temp <- data.frame(
-#     warmest_month = df[which(df$tmax_monthly ==
-#                                max(df$tmax_monthly)), ]$month_of,
-#     coldest_month =  df[which(df$tmin_monthly ==
-#                                 min(df$tmin_monthly)), ]$month_of,
-#     wettest_month = df[which(df$rain_monthly_mm ==
-#                                max(df$rain_monthly_mm)), ]$month_of,
-#     driest_month = df[which(df$rain_monthly_mm ==
-#                               min(df$rain_monthly_mm)), ]$month_of
-#   )
-#
-#   return(temp)
-# }
-#
-# # Monthly extremes
-# monthly_extremes <- monthly_clim_means %>%
-#   ungroup() %>%
-#   group_by(site) %>%
-#   do(mon_extremes(.))
-#
-#
-# # Quarterly
-# prec <- monthly_clim_means %>%
-#   select(-tmin_monthly, -tmax_monthly) %>%
-#   spread(month_of, rain_monthly_mm) %>%
-#   select(-site)
-#
-# tmax <- monthly_clim_means %>%
-#   select(-rain_monthly_mm, -tmin_monthly) %>%
-#   spread(month_of, tmax_monthly) %>%
-#   select(-site)
-#
-# tmin <- monthly_clim_means %>%
-#   select(-rain_monthly_mm, -tmax_monthly) %>%
-#   spread(month_of, tmin_monthly) %>%
-#   select(-site)
-#
-# # Calculate hottest/coldest/wettest/driest quarters for each site
-# qua <- data.frame(extremes(tmin = tmin, tmax = tmax, prec = prec))
-# qua$site <- monthly_extremes$site
-#
-# quarterly_extremes <- qua %>%
-#   mutate_each(funs(month.abb[.]), -site) %>%
-#   select(site, 1:4)
-#
-# names(quarterly_extremes)[-1] %<>% paste0("_quarter")
-# names(quarterly_extremes)[-1] <- tolower(names(quarterly_extremes)[-1])
-#
-# clim_extremes <- inner_join(monthly_extremes, quarterly_extremes) %>%
-#   gather(variable, month_of, -site) %>%
-#   arrange(site, variable)
+# Calculate hottest/coldest/wettest/driest months for each site
+monthly_clim_means <- climates %>%
+  ungroup() %>%
+  group_by(site, month_of) %>%
+  summarise_each(funs(mean), tmin_monthly, tmax_monthly, rain_monthly_mm, tavg_monthly)
+
+
+mon_extremes <- function(df){
+
+  temp <- data.frame(
+    warmest_month = df[which(df$tmax_monthly ==
+                               max(df$tmax_monthly)), ]$month_of,
+    coldest_month =  df[which(df$tmin_monthly ==
+                                min(df$tmin_monthly)), ]$month_of,
+    wettest_month = df[which(df$rain_monthly_mm ==
+                               max(df$rain_monthly_mm)), ]$month_of,
+    driest_month = df[which(df$rain_monthly_mm ==
+                              min(df$rain_monthly_mm)), ]$month_of
+  )
+
+  return(temp)
+}
+
+# Monthly extremes
+monthly_extremes <- monthly_clim_means %>%
+  ungroup() %>%
+  group_by(site) %>%
+  do(mon_extremes(.))
+
+
+# Quarterly
+tmin <- monthly_clim_means %>%
+  ungroup() %>%
+  select(site, month_of, tmin_monthly) %>%
+  spread(month_of, tmin_monthly) %>%
+  select(-site)
+
+tmax <- monthly_clim_means %>%
+  ungroup() %>%
+  select(site, month_of, tmax_monthly) %>%
+  spread(month_of, tmax_monthly) %>%
+  select(-site)
+
+prec <- monthly_clim_means %>%
+  ungroup() %>%
+  select(site, month_of, rain_monthly_mm) %>%
+  spread(month_of, rain_monthly_mm) %>%
+  select(-site)
+
+tavg <- monthly_clim_means %>%
+  ungroup() %>%
+  select(site, month_of, tavg_monthly) %>%
+  spread(month_of, tavg_monthly) %>%
+  select(-site)
+
+# Calculate hottest/coldest/wettest/driest quarters for each site
+qua <- data.frame(extremes(tmin = tmin, tmax = tmax, prec = prec, tmean = tavg))
+qua$site <- monthly_extremes$site
+
+quarterly_extremes <- qua %>%
+  mutate_each(funs(factor(month.abb[.], levels = month.abb)), -site) %>%
+  select(site, 1:4)
+
+names(quarterly_extremes)[-1] <- names(quarterly_extremes)[-1] %>% paste0("_quarter")
+names(quarterly_extremes)[-1] <- tolower(names(quarterly_extremes)[-1])
+
+clim_extremes <- inner_join(monthly_extremes, quarterly_extremes) %>%
+  data.frame()
+
+row.names(clim_extremes) <- clim_extremes$site
+clim_extremes <- clim_extremes[, -1]
+
+
 
 source('analysis_scripts/bioclim.R')
 
-get_bioclim <- function(df){
-
-  tmin <- df %>%
-    ungroup() %>%
-    select(-year_of, -rain_monthly_mm, -tmax_monthly) %>%
-    spread(month_of, tmin_monthly) %>%
-    select(-site)
-
-  tmax <- df %>%
-    ungroup() %>%
-    select(-year_of, -rain_monthly_mm, -tmin_monthly) %>%
-    spread(month_of, tmax_monthly) %>%
-    select(-site)
-
-  prec <- df %>%
-    ungroup() %>%
-    select(-year_of, -tmin_monthly, -tmax_monthly) %>%
-    spread(month_of, rain_monthly_mm) %>%
-    select(-site)
-
-  b <- data.frame(suppressMessages(bioclim(tmin, tmax, prec, t.as.int = FALSE)))
-  names(b)[1] <- "value"
-  b$variable <- row.names(b)
-  b <- b %>% select(variable, value)
-
-  row.names(b) <- NULL
-
-  return(b)
-
-}
-
 # Bioclim
-temp <- climates %>%
+bioclim_df <- climates %>%
   inner_join(select(ann_total, site, year_of, n_months)) %>%
   group_by(site, year_of) %>%
-  select(month_of, rain_monthly_mm, tmin_monthly, tmax_monthly) %>%
-  do(get_bioclim(.))
+  select(month_of, tmin_monthly, tmax_monthly, rain_monthly_mm, tavg_monthly) %>%
+  do(get_bioclim_annual(., clim_extremes))
 
-temp$variable <- mapvalues(temp$variable,
+bioclim_df$variable <- mapvalues(bioclim_df$variable,
                            from = c("bioclim_1", "bioclim_2", "bioclim_3",
                                     "bioclim_4", "bioclim_5", "bioclim_6",
                                     "bioclim_7", "bioclim_8", "bioclim_9",
@@ -107,28 +89,29 @@ temp$variable <- mapvalues(temp$variable,
                                     "bioclim_13", "bioclim_14", "bioclim_15",
                                     "bioclim_16", "bioclim_17", "bioclim_18",
                                     "bioclim_19"),
-                           to = c("01_annual_mean_temp", "02_mean_diurnal_range",
-                                  "03_isothermality", "04_temp_seasonality",
-                                  "05_max_temp_warmest_m", "06_min_temp_coldest_m",
-                                  "07_temp_annual_range", "08_mean_temp_wettest_q",
-                                  "09_mean_temp_driest_q", "10_mean_temp_warmest_q",
-                                  "11_mean_temp_coldest_q", "12_annual_precip",
-                                  "13_precip_wettest_m", "14_precip_driest_m",
-                                  "15_precip_seasonality", "16_precip_wettest_q",
-                                  "17_precip_driest_q", "18_precip_warmest_q",
-                                  "19_precip_coldest_q"))
+                           to = c("v01_annual_mean_temp", "v02_mean_diurnal_range",
+                                  "v03_isothermality", "v04_temp_seasonality",
+                                  "v05_max_temp_warmest_m", "v06_min_temp_coldest_m",
+                                  "v07_temp_annual_range", "v08_mean_temp_wettest_q",
+                                  "v09_mean_temp_driest_q", "v10_mean_temp_warmest_q",
+                                  "v11_mean_temp_coldest_q", "v12_annual_precip",
+                                  "v13_precip_wettest_m", "v14_precip_driest_m",
+                                  "v15_precip_seasonality", "v16_precip_wettest_q",
+                                  "v17_precip_driest_q", "v18_precip_warmest_q",
+                                  "v19_precip_coldest_q"))
 
-clim_extremes <- spread(temp, variable, value)
+temp <- spread(bioclim_df, variable, value)
 
 ggplot() +
-  geom_tile(data = clim_extremes,
+  geom_tile(data = temp,
             aes(x = site, y = year_of,
-                fill = bioclim_8)) +
-  scale_fill_gradientn(colours = brewer.pal(9, "YlOrRd"),
-                       name = "Max Temp of Warmest Month") +
+                fill = v19_precip_coldest_q)) +
+  scale_fill_gradientn(colours = brewer.pal(9, "Blues"),
+                       name = "Precip of Coldest Quarter",
+                       trans = sqrt_trans()) +
   # facet_grid(. ~ site) +
   theme_bw() +
-  labs(x = "Month", y = "Year", title = "Max Temp of Warmest Month\n") +
+  labs(x = "Month", y = "Year", title = "Precip of Coldest Quarter\n") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         legend.position = "bottom",
         strip.background = element_blank(),
@@ -137,3 +120,15 @@ ggplot() +
         legend.key.width = unit(4, "cm"),
         panel.margin = unit(1, "lines")) +
   scale_y_continuous(limits = c(1955, 2016), breaks = seq(1955, 2015, by = 5))
+
+
+
+
+# ---- ind_clim -----------------------------------------------------------
+
+indclim_df <- ind_df %>%
+  mutate(year_of = year(date_of),
+         month_of = month(date_of)) %>%
+  group_by(index, year_of) %>%
+  filter(year_of < 2015) %>%
+  do(get_indclim(., tmin, tmax, prec, tavg))
